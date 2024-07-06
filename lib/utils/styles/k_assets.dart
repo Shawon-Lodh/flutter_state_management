ae5cf1ebc66fec2478759652cbc4e4059db10749
shawon <shawon.lodh@sslwireless.com>
2024-07-06 13:34:27 +0600

    feat: add assets

--- lib/utils/styles/k_assets.dart
+++ lib/utils/styles/k_assets.dart
@@ -1,23 +1,44 @@
 enum KAssetName {
-  oil,
-  close_bottom,
+  icArrowBackSvg,
+  icChevronLeftSvg,
+  icChevronRightSvg,
+  icCloseSvg,
+  icPokeballSvg,
+  icScaleSvg,
+  icSearchSvg,
+  icSortSvg,
+  icTagSvg,
+  icTextFormatSvg,
+  icWeightSvg,
 }
 
-extension AssetsExtention on KAssetName {
+extension AssetsExtension on KAssetName {
   String get imagePath {
-    String _rootPath = 'assets';
-    String _svgDir = '$_rootPath/svg';
-    String _imageDir = '$_rootPath/images';
-
+    const String _rootPath = 'assets';
+  const String _svgDir = '$_rootPath/svg';
     switch (this) {
-      case KAssetName.oil:
-        return "$_imageDir/oil.png";
-      case KAssetName.close_bottom:
-        return "$_svgDir/close_bottom.svg";
-
-      default:
-        return "";
+      case KAssetName.icArrowBackSvg:
+        return '$_svgDir/ic_arrow_back.svg';
+      case KAssetName.icChevronLeftSvg:
+        return '$_svgDir/ic_chevron_left.svg';
+      case KAssetName.icChevronRightSvg:
+        return '$_svgDir/ic_chevron_right.svg';
+      case KAssetName.icCloseSvg:
+        return '$_svgDir/ic_close.svg';
+      case KAssetName.icPokeballSvg:
+        return '$_svgDir/ic_pokeball.svg';
+      case KAssetName.icScaleSvg:
+        return '$_svgDir/ic_scale.svg';
+      case KAssetName.icSearchSvg:
+        return '$_svgDir/ic_search.svg';
+      case KAssetName.icSortSvg:
+        return '$_svgDir/ic_sort.svg';
+      case KAssetName.icTagSvg:
+        return '$_svgDir/ic_tag.svg';
+      case KAssetName.icTextFormatSvg:
+        return '$_svgDir/ic_text_format.svg';
+      case KAssetName.icWeightSvg:
+        return '$_svgDir/ic_weight.svg';
     }
   }
 }
-
